58de2c2caf49f3e501e6776bdc0796e43e6144d8
armenaghajanyan <78469919+armenaghajanyan@users.noreply.github.com>
2021-02-04 00:33:19 +0400

    contact us with responsive and animation

--- js/index.js
+++ js/index.js
@@ -1,16 +1,22 @@
 function showNav() {
-    const navOpen = document.getElementsByClassName("burger-nav-bar")[0];
+    const navOpen = document.querySelector('.burger-nav-bar');
     if (!navOpen.style.animationName || navOpen.style.animationName === "nav-bar-close") {
         navOpen.style.animationName = "nav-bar-open";
     } else {
         navOpen.style.animationName = "nav-bar-close";
     }
 }
-
-addEventListener("resize", () => {
+window.addEventListener("click", (e) => {
+    const navOpen = document.querySelector('.burger-nav-bar');
+    if(!e.composedPath().includes(document.querySelector(".burger")) && navOpen.style.animationName === "nav-bar-open") {
+        navOpen.style.animationName = "nav-bar-close";
+    }
+})
+window.addEventListener("resize", () => {
         const navOpen = document.getElementsByClassName("burger-nav-bar")[0];
         if(window.innerWidth > 1024) {
             navOpen.style.animationName = "";
         }
     }
 )
+
